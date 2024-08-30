62632e9b1048cea148c6dc36384820a5052e5b8d
iamseeley <seeleypersonal@gmail.com>
2024-08-30 08:47:34 -0400

    adding exercise 1.23

--- ch1/1.23.scm
+++ ch1/1.23.scm
@@ -6,13 +6,25 @@
  (define (find-divisor n test-divisor) 
    (cond ((> (square test-divisor) n) n) 
          ((divides? test-divisor n) test-divisor) 
-         (else (find-divisor n (+ test-divisor 1))))) 
+         (else (find-divisor n (next test-divisor)))))
+
+
   
  (define (divides? a b) 
-   (= (remainder b a) 0)) 
-  
- (define (prime? n) 
-   (= n (smallest-divisor n))) 
+   (= (remainder b a) 0))
+
+; new next
+
+(define (next n)
+  (if (= n 2) 
+    3
+    (+ n 2)))
+
+
+(define (prime? n) 
+   (= n (smallest-divisor n)))
+
+
 (define (timed-prime-test n)
   (newline)
   (display n)
@@ -26,27 +38,7 @@
   (display " *** ")
   (display elapsed-time))
 
-(define (search-for-primes lower upper)
-  (define (iter n)
-    (cond ((<= n upper) (timed-prime-test n) (iter (+ n 2)))))
-  (iter (if (odd? lower) lower (+ lower 1))))
 
- (search-for-primes 1000 1019)       ; 1e3 
- (search-for-primes 10000 10037)     ; 1e4 
- (search-for-primes 100000 100043)   ; 1e5 
- (search-for-primes 1000000 1000037) ; 1e6
-
-
- (newline) 
- (search-for-primes 1000000000 1000000021)       ; 1e9 
- (search-for-primes 10000000000 10000000061)     ; 1e10 
- (search-for-primes 100000000000 100000000057)   ; 1e11 
- (search-for-primes 1000000000000 1000000000063) ; 1e12
-
-
-
-
-(newline)
 (timed-prime-test 1000000007)
 (timed-prime-test 1000000009)
 (timed-prime-test 1000000021)
@@ -59,3 +51,6 @@
 (timed-prime-test 1000000000039)
 (timed-prime-test 1000000000061)
 (timed-prime-test 1000000000063)
+
+
+; using the new next procedure does make the algorithm run about twice as fast. closer to 1.5 times as fast. the input did halve, but we still do an extra if test.
